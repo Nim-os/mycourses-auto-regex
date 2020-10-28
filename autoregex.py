4d402dec470357f478497c55a2f046230e4414b5
Christophe Simon <10841304+Nim-os@users.noreply.github.com>
2020-10-27 22:39:10 -0400

    Add REGEX character prevention

--- autoregex.py
+++ autoregex.py
@@ -1,5 +1,9 @@
 ans = input("Answer: ")
 
+antiRegexSquad = ["$","^","*","+","|",".","?","(",")","[","]","{","}"]
+
+
+
 ans = ans.strip()
 ans = " ".join(ans.split()) # Remove duped spaces
 
@@ -8,17 +12,25 @@ regexAns = "" # Final regex-ed answer
 lastChar = ""
 
 for x in ans:
-  if(x == "(" or x == ")"):
-    cur = "[\s]*\\" + x + "[\s]*"
-  else:
+    
     cur = x
+    
+    # Prevent these characters from being used as REGEX
+    if(x in antiRegexSquad):
+        if(x == ".")
+            cur = "\\" + x
+        else:
+            cur = "[\s]*\\" + x + "[\s]*"
   
-  if(lastChar == " "):
-    regexAns += "[\s]*"
-  lastChar = x
-  if(lastChar == " "):
-    continue
-  regexAns += cur
+    if(lastChar == " "):
+        regexAns += "[\s]*"
+        
+    lastChar = x
+    
+    if(lastChar == " "):
+        continue
+    
+    regexAns += cur
   
 print("Regex Answer: ", regexAns)
 
